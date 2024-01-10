d4a4fa2b89d695b8648e66f43309fd625bd90be6
Maurilio Sousa <maurilio.sousa@quintoandar.com.br>
2024-01-10 02:21:07 -0300

    create integration with odmapi

--- src/main/java/start/spring/io/screenmatch/ScreenmatchApplication.java
+++ src/main/java/start/spring/io/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,22 @@
 package start.spring.io.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import start.spring.io.screenmatch.service.ConsumoApi;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=f73a26de");
+		System.out.println(json);
 	}
 
 }

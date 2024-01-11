9618da2d27cebe6799a12997149a980204b2c9bc
Maurilio Sousa <maurilio.sousa@quintoandar.com.br>
2024-01-11 12:53:49 -0300

    creating service to deserialise json

--- src/main/java/start/spring/io/screenmatch/ScreenmatchApplication.java
+++ src/main/java/start/spring/io/screenmatch/ScreenmatchApplication.java
@@ -3,7 +3,9 @@ package start.spring.io.screenmatch;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import start.spring.io.screenmatch.model.Serie;
 import start.spring.io.screenmatch.service.ConsumoApi;
+import start.spring.io.screenmatch.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,7 +18,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=f73a26de");
-		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		Serie serie = conversor.obterDados(json, Serie.class);
+		System.out.println(serie);
 	}
 
 }
--- src/main/java/start/spring/io/screenmatch/model/Serie.java
+++ src/main/java/start/spring/io/screenmatch/model/Serie.java
@@ -1,7 +1,9 @@
 package start.spring.io.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
   public record Serie(@JsonAlias("Title") String titulo,
                       @JsonAlias("totalSeasons") Integer totalTemporadas,
                       @JsonAlias("imdbRating") String avaliacao) {
